898f72dd680a921f86973fd22ba141793df8839d
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2024-04-16 14:28:20 +0200

    improve DoubleLock example

--- src/main/kotlin/org/jetbrains/kotlin/public/course/parallel/DoubleLock.kt
+++ src/main/kotlin/org/jetbrains/kotlin/public/course/parallel/DoubleLock.kt
@@ -1,16 +1,36 @@
 package org.jetbrains.kotlin.public.course.parallel
 
-import kotlin.concurrent.thread
-
 class ComplicatedThing {
     var value = 3
 
     init {
+        /**
+         * Imagine that this is actually a very complicated constructor.
+         */
         Thread.sleep(value * 1000L)
         value = 23
     }
 }
 
+/**
+ * We want a thread-safe singleton, which is instantiated only when needed (might not happen at all).
+ * There are obvious problems with this solution in a multithreaded application.
+ */
+object SingleThingyBroken {
+    private var thingy: ComplicatedThing? = null
+
+    fun getThingy(): ComplicatedThing {
+        if (thingy == null) {
+            thingy = ComplicatedThing()
+        }
+        return checkNotNull(thingy)
+    }
+}
+
+/**
+ * We may try to fix the problem by moving the Thingy construction into a synchronized section.
+ * In theory, one thread can start creating the Thingy, while another might see intermediary state: value of 3.
+ */
 object DoubleCheckLockAntiPattern {
     private var thingy: ComplicatedThing? = null
 
@@ -27,19 +47,24 @@ object DoubleCheckLockAntiPattern {
 }
 
 /**
- * In theory the first thread can start creating the thingy, while the second might see intermediary state: value of 3.
- * @Volatile fixes that
- * by lazy {} make it even better
+ * `@Volatile` fixes that by making read happen only after constructor finished.
+ * `by lazy {}` makes it even better, thanks Kotlin!
  */
-fun main() {
-    val t1 = thread {
-        val v = DoubleCheckLockAntiPattern.getThingy().value
-        println(v)
+object Better {
+    @Volatile
+    private var thingy1: ComplicatedThing? = null
+    private val thingy2: ComplicatedThing? by lazy {
+        ComplicatedThing()
     }
-    val t2 = thread {
-        val v = DoubleCheckLockAntiPattern.getThingy().value
-        println(v)
+
+    fun getThingy1(): ComplicatedThing {
+        if (thingy1 == null) {
+            synchronized(this) {
+                if (thingy1 == null) {
+                    thingy1 = ComplicatedThing()
+                }
+            }
+        }
+        return checkNotNull(thingy1)
     }
-    t1.join()
-    t2.join()
 }
